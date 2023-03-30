f7897532388db0c5bd557dc8948235b298655255
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-30 10:33:27 +0200

    reports_soilluns initial commit

---
+++ reports_soilluns/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Reports Soilluns",
+    "summary": "Reports form sale order and invoice.",
+    "version": "14.0.1.0.0",
+    "category": "Reports",
+    "author": "Pedro Guirao, Antonio Lopez ",
+    "website": "",
+    "license": "AGPL-3",
+    "depends": ['account'],
+    "data": [
+        "views/report.xml",
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
